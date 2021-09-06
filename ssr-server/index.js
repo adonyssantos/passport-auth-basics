c9e05334418c7ad258642943d0d0c5d921c6b4cd
Adonys Santos <adonyssantosjimenez@gmail.com>
2021-09-06 00:02:37 -0400

    Add userMovies routes

--- ssr-server/index.js
+++ ssr-server/index.js
@@ -20,7 +20,7 @@ app.post('/auth/sign-in', async (request, response, next) => {
   passport.authenticate('basic', (error, data) => {
     try {
       if (error.error || !data) {
-        return boom.unauthorize();
+        return boom.unauthorized();
       }
 
       request.login(data, { session: false }, error => {
@@ -48,7 +48,7 @@ app.post('/auth/sign-up', async (request, response, next) => {
 
   try {
     await axios({
-      url: `${config.apiUrl}/auth/sign-up`,
+      url: `${config.apiUrl}/api/auth/sign-up`,
       method: 'POST',
       data: user,
     });
@@ -61,9 +61,52 @@ app.post('/auth/sign-up', async (request, response, next) => {
 
 app.get('/movies', async (request, response, next) => {});
 
-app.post('/user-movies', async (request, response, next) => {});
+app.post('/user-movies', async (request, response, next) => {
+  try {
+    const { body: userMovies } = request;
+    const { token } = request.cookies;
 
-app.delete('/user-movies/:userMovieId', async (request, response, next) => {});
+    const { data, status } = await axios({
+      url: `${config.apiUrl}/api/user-movies`,
+      Headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      method: 'POST',
+      data: userMovies,
+    });
+
+    if (status !== 201) {
+      return next(boom.badImplementation());
+    }
+
+    response.status(201).json(data);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.delete('/user-movies/:userMovieId', async (request, response, next) => {
+  try {
+    const { userMovieId } = request.params;
+    const { token } = request.cookies;
+
+    const { data, status } = await axios({
+      url: `${config.apiUrl}/api/user-movies/${userMovieId}`,
+      Headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      method: 'delete',
+    });
+
+    if (status !== 200) {
+      return next(boom.badImplementation());
+    }
+
+    response.status(200).json(data);
+  } catch (error) {
+    next(error);
+  }
+});
 
 app.listen(config.port, () => {
   console.log(`Listening http://localhost:${config.port}`);
